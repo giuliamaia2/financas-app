c9dd04b89f84616fe9e3b17bc5c49e754bcb078b
giuliamaia2 <giuliamaia2@gmail.com>
2020-03-13 15:55:08 -0300

    Subindo rotas e componentes de cadastro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //arquivo de entrada da aplicação react
 import React from 'react';
 import ReactDOM from 'react-dom'; //componente de renderização web
-import App from './App';
+import App from './main/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/views/cadastro-usuario.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
+
+
+class CadastroUsuario extends React.Component {
+
+
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Card title="Cadastro de Usuário">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <fieldset>
+                                    <FormGroup label="Nome: *" htmlFor="inputNome">
+                                        <input type="text"
+                                            className="form-control"
+                                            id="inputNome"
+                                            name="nome"
+                                            onChange={e => this.setState({ nome: e.target.value })}
+                                            placeholder="Digite o Nome" />
+                                    </FormGroup>
+                                    <FormGroup label="Email: *" htmlFor="inputEmail">
+                                        <input type="email"
+                                            className="form-control"
+                                            id="inputEmail"
+                                            name="email"
+                                            onChange={e => this.setState({ email: e.target.value })}
+                                            placeholder="Digite o Email" />
+                                    </FormGroup>
+                                    <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                        <input type="password"
+                                            className="form-control"
+                                            id="inputSenha"
+                                            name="senha"
+                                            onChange={e => this.setState({ senha: e.target.value })}
+                                            placeholder="Digite a Senha" />
+                                    </FormGroup>
+                                    <FormGroup label="Confirme a Senha: *" htmlFor="inputRepitaSenha">
+                                        <input type="password"
+                                            className="form-control"
+                                            id="inputRepitaSenha"
+                                            name="senha"
+                                            onChange={e => this.setState({ senhaRepeticao: e.target.value })}
+                                            placeholder="Digite novamente a senha" />
+                                    </FormGroup>
+                                </fieldset>
+                                <button onClick={this.cadastrar} type="button" className="btn btn-success">Salvar</button>
+                                <button type="button" className="btn btn-danger">Cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default CadastroUsuario
