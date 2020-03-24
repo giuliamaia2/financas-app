b510f74557b5075fd57d57e7faba5840ee7b3ae4
giuliamaia2 <giuliamaia2@gmail.com>
2020-03-23 21:22:30 -0300

    primeiras chamadas de api

--- src/views/login.js
+++ src/views/login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends React.Component {
 
@@ -11,7 +12,14 @@ class Login extends React.Component {
     }
 
     entrar = () => {
-        console.log('ENTREI');
+        axios.post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+        }).then(response => {
+            console.log(response)
+        }).catch(erro => {
+            console.log(erro.response)
+        })
     }
 
     prepareCadastrar = () => {
